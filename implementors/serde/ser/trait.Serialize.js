705df4241f4e3cf2d21754e260ce617ebd56a295
insanitybit <insanitybit@gmail.com>
2015-07-22 23:31:03 +0000

    rebuild pages at c8c4549

---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['serde'] = [];implementors['cricket'] = ["impl <a class='trait' href='serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='cricket/struct.AFLView.html' title='cricket::AFLView'>AFLView</a>","impl <a class='trait' href='serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='cricket/struct.History.html' title='cricket::History'>History</a>","impl <a class='trait' href='serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='cricket/struct.AFLOpts.html' title='cricket::AFLOpts'>AFLOpts</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
