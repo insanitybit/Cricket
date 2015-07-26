55864ef24b2529695342753ca975ac1b782aef2f
insanitybit <insanitybit@gmail.com>
2015-07-26 13:25:07 +0000

    rebuild pages at 6f5572e

--- implementors/std/sys/ext/io/trait.AsRawFd.js
+++ implementors/std/sys/ext/io/trait.AsRawFd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['openssl'] = [];
+implementors['openssl'] = [];implementors['hyper'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='hyper/net/struct.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>",];implementors['iron'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='hyper/net/struct.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>",];implementors['nickel'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='hyper/net/struct.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
